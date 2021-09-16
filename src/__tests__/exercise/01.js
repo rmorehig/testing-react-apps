b153dace717f10d10005926b39d397691bc040ea
Rafa Moreno <rmh1516@gmail.com>
2021-09-16 20:02:16 +0200

    finish exercise 2

--- src/__tests__/exercise/01.js
+++ src/__tests__/exercise/01.js
@@ -14,7 +14,6 @@ test('counter increments and decrements when the buttons are clicked', () => {
   document.body.append(div)
   ReactDOM.render(<Counter />, div)
   const [decrement, increment] = div.querySelectorAll('button')
-
   const message = div.firstElementChild.querySelector('div')
   expect(message.textContent).toBe('Current count: 0')
   const incrementClickEvent = new MouseEvent('click', {
