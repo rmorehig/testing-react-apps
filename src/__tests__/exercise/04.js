b52a50d27078638253b058985207069ca4e92f5e
Rafa Moreno <rmh1516@gmail.com>
2021-09-17 09:55:47 +0200

    finish exercise 4

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -4,27 +4,31 @@
 import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+import {build, fake} from '@jackfranklin/test-data-bot'
 import Login from '../../components/login'
 
-test('submitting the form calls onSubmit with username and password', () => {
-  // 🐨 create a variable called "submittedData" and a handleSubmit function that
-  // accepts the data and assigns submittedData to the data that was submitted
-  // 💰 if you need a hand, here's what the handleSubmit function should do:
-  // const handleSubmit = data => (submittedData = data)
-  //
-  // 🐨 render the login with your handleSubmit function as the onSubmit prop
-  //
-  // 🐨 get the username and password fields via `getByLabelText`
-  // 🐨 use userEvent.type to change the username and password fields to
-  //    whatever you want
-  //
-  // 🐨 click on the button with the text "Submit"
-  //
-  // assert that submittedData is correct
-  // 💰 use `toEqual` from Jest: 📜 https://jestjs.io/docs/en/expect#toequalvalue
+const buildLoginForm = build({
+  fields: {
+    username: fake(f => f.internet.userName()),
+    password: fake(f => f.internet.password()),
+  },
 })
 
-/*
-eslint
-  no-unused-vars: "off",
-*/
+test('submitting the form calls onSubmit with username and password', () => {
+  const handleSubmit = jest.fn()
+  render(<Login onSubmit={handleSubmit} />)
+  const {password, username} = buildLoginForm()
+  const usernameInput = screen.getByRole('textbox', {name: /username/i})
+  const passwordInput = screen.getByLabelText(/password/i)
+  userEvent.type(usernameInput, username)
+  userEvent.type(passwordInput, password)
+
+  const button = screen.getByRole('button', {name: /submit/i})
+  userEvent.click(button)
+
+  expect(handleSubmit).toHaveBeenCalledTimes(1)
+  expect(handleSubmit).toHaveBeenCalledWith({
+    username,
+    password,
+  })
+})
